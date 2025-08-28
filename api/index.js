48409c600622b92d58b77511647d567bd5a9d870
degamAljune <degamoaljune14@gmail.com>
2025-08-28 20:01:59 +0800

    Initial commit: InReal server deployment comment remove

--- api/index.js
+++ api/index.js
@@ -20,15 +20,7 @@ const cors = require("cors");
 //     }
 // }
 
-// let prisma;
-// if (process.env.NODE_ENV === "production") {
-//     prisma = new PrismaClient();
-// } else {
-//     if (!global.prisma) {
-//         global.prisma = new PrismaClient();
-//     }
-//     prisma = global.prisma;
-// }
+
 
 const app = express();
 // Middleware
@@ -51,44 +43,7 @@ app.get("/api/health", (req, res) => {
     });
 });
 
-// // GET /users - List all users
-// app.get("/api/users", async (req, res) => {
-//   try {
-//     const users = await prisma.users.findMany({
-//       select: {
-//         id: true,
-//         first_name: true,
-//         last_name: true,
-//         email: true,
-//         place: true,
-//         user_type: true,
-//         preferences: true,
-//         verify: true,
-//         verification_token: true,
-//         verified_at: true,
-//         image: true,
-//         qr_code: true,
-//         created_at: true,
-//         updated_at: true,
-//         deleted_at: true,
-//       },
-//     });
-//     res.json(users);
-//   } catch (error) {
-//     console.error("❌ Error fetching users:", error);
-//     res.status(500).json({ error: "Failed to fetch users" });
-//   }
-// });
 
-// Use it under /users
-// const userRoutes = require("../modules/user");
-// app.use("/api/users", userRoutes);
-
-
-// ✅ 404 handler
-app.use("*", (req, res) => {
-    res.status(404).json({ error: "Endpoint not found" });
-});
 
 // if (require.main === module) {
 //     const PORT = process.env.PORT || 8080;
